367a5da62e3e74b3f4ebb281ca60420fe7f35b7b
Marcos-San <115675567+Marcos-San@users.noreply.github.com>
2025-11-29 17:40:41 -0300

    Inicio do projeto

---
+++ projeto_logistica/logistica/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100),
+    email = models.EmailField(),
+    telefone = models.IntegerField()
+
+class Entrega(models.Model):
+    STATUS_ENTREGA = [
+        ('pendente', 'Pendente'),
+        ('em_transito', 'Em trânsito'),
+        ('entregue', 'Entregue'),
+        ('cancelada', 'Cancelada'),
+        ('remarcada', 'Remarca'),
+    ]
+    codigo_rastreio = models.IntegerField(unique=True),
+    cliente = models.ForeignKey(Cliente),
+    endereco_origem = models.CharField(max_length=100),
+    cep_origem = models.IntegerField(max_length=8),
+    endereco_destino = models.CharField(max_length=100),
+    cep_destino = models.IntegerField(max_length=8),
+    status = models.CharField(max_length=10, choices=STATUS_ENTREGA, default='pendente')
+    capacidade_necessaria = models.FloatField(),
+    valor_frete = models.FloatField(),
+    data_solicitacao = models.DateField(),
+    data_entrega_prevista = models.DateField(),
+    data_entrega_real = models.DateField(),
+    obs = models.TextField(),
+
+    def __str__(self):
+        return self.codigo_rastreio
+
+class Motorista(models.Model):
+    STATUS_MOTORISTA = [
+        ('ativo', 'Ativo'),
+        ('inativo', 'Inativo'),
+        ('em_rota', 'Em Rota'),
+        ('disponível', 'Disponivel'),
+    ]
+    nome = models.CharField(max_length=100)
+    cpf = models.IntegerField(max_length=11,unique=True)
+    cnh = models.CharField(max_length=100)
+    telefone = models.IntegerField(max_length=11)
+    status = models.CharField(max_length=100, choices=STATUS_MOTORISTA, default='ativo')
+    data_cadastro = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Veiculo(models.Model):
+    STATUS_VEICULO = [
+    ('disponivel', 'Disponível'),
+    ('em_uso', 'Em uso'),
+    ('manutencao', 'Manutenção'),
+    ]
+    TIPO_VEICULO = [
+        ('carro', 'Carro'),
+        ('van', 'Van'),
+        ('caminha', 'Caminhão'),
+    ]
+    placa = models.CharField(max_length=7),
+    modelo = models.CharField(max_length=100),
+    tipo = models.CharField(max_length=20, choices=TIPO_VEICULO),
+    capacidade_maxima = models.FloatField(),
+    km_atual = models.IntegerField(),
+    status = models.CharField(max_length=50, choices=STATUS_VEICULO, default='disponivel'),
+    motorista = models.ForeignKey(Motorista)
+
+    def __str__(self):
+        return self.placa
+
+class Rota(models.Model):
+    STATUS_ROTA = [
+        ('planejada','Planejada'),
+        ('em_andamento', 'Em andamento'),
+        ('concluida', 'Concluida'),
+    ]
+    nome = models.CharField(max_length=100),
+    descricao = models.TextField(),
+    entrega = models.ForeignKey(Entrega),
+    motorista = models.ForeignKey(Motorista),
+    veiculo = models.ForeignKey(Veiculo),
+    data_rota = models.DateField(),
+    status = models.CharField(max_length=20, choices=STATUS_ROTA, default='planejada'),
+    capacidade_total_utilizada = models.FloatField(),
+    km_total_estimado = models.IntegerField(),
+    tempo_estimado = models.DateField()
+
+    def __str__(self):
+        return self.nome
+

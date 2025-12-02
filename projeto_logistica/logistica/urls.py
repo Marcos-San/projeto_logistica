fdbefeb629bb56cbc0516cdcb266ba3291cc08a9
Marcos-San <115675567+Marcos-San@users.noreply.github.com>
2025-12-02 16:52:04 -0300

    Pagina home e login

--- projeto_logistica/logistica/urls.py
+++ projeto_logistica/logistica/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import custom_logout
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('index/', views.index, name='index'),
 
     path(
         'login/',
@@ -14,11 +16,5 @@ urlpatterns = [
         ),
         name='login'
         ),
-    path(
-        'logout/',
-        auth_views.LogoutView.as_view(
-            template_name='users/logout.html'
-        ),
-        name='logout'
-        ),
+    path('logout/', custom_logout, name='logout')
 ]
--- projeto_logistica/logistica/views.py
+++ projeto_logistica/logistica/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from .models import *
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth import logout
 
 def home(request):
     return render(request, 'log/home.html')
 
+def index(request):
+    return render(request, 'log/index.html')
+
+def custom_logout(request):
+    logout(request)
+    return redirect('home')
 
 def lista_motorista(request):
     motoristas = Motorista.objects.all()
